2f5a83165a002902aeac1377eee1943d364729a2
ElenaHrndz <45853166+ElenaHrndz@users.noreply.github.com>
2019-01-15 14:52:05 -0600

    JS code
    start of js code

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,32 @@
 window.cipher = {
-
+  encode: (numberCode, textToEncode) => {
+    code(numberCode, textToEncode);
+  },
+  decode: (numberCode, textToDecode) => {
+    decode(numberCode, textToDecode);
+  },
 };
+
+let maxLetterNumber = 26;
+
+function code(number, text:textToConver) {
+  let newNumber = numberMoves(number);
+  let textToConvert = text.toUpperCase();
+  for (let i; i < textToConvert; i++) {
+    let letterAscii = textToConvert.charCodeAt(i);
+    let codeform = (letterAscii + 65 - offset) % 26 + 65;
+    code += string.fromCharCode(letterAscii[i]);
+  }
+}
+
+
+function decode(number, text) {
+  let newNumber = numberMoves(number);
+
+}
+
+function numberMoves(numberToConvert) {
+  numberToConvert = parseInt(numberToConvert);
+  numberToConvert %= maxLetterNumber;
+  return numberToConvert;
+}
---
+++ src/index.js
@@ -0,0 +1,10 @@
+let messageTo = document.getElementById("yourInput");
+let number = document.getElementById("numberCode");
+let coder = new cipher();
+
+let result = coder.endcode(number, messageTo);
+
+
+function cambiarTextos(){
+			texto.innerHTML=texto.innerHTML=="Mensaje"?"Codigo":"Mensaje";
+		}
