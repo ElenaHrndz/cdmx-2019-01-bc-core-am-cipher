783c7834b0ffca33c075da666dd657adeab51595
Elena <hrndzelena@gmail.com>
2019-01-16 20:43:18 -0600

    Funtional page
    cipher.js and index.js are working properly.
    Pass the first general test

--- src/cipher.js
+++ src/cipher.js
@@ -1,29 +1,54 @@
-window.cipher = {
-  encode: (numberCode, textToEncode) => {
-    code(numberCode, textToEncode);
-  },
-  decode: (numberCode, textToDecode) => {
-    decode(numberCode, textToDecode);
-  },
-};
-
 let maxLetterNumber = 26;
 
-function code(number, text:textToConver) {
-  let newNumber = numberMoves(number);
-  let textToConvert = text.toUpperCase();
-  for (let i; i < textToConvert; i++) {
-    let letterAscii = textToConvert.charCodeAt(i);
-    let codeform = (letterAscii + 65 - offset) % 26 + 65;
-    code += string.fromCharCode(letterAscii[i]);
+window.cipher = {
+  encode: (totalNumberCypher, textToConvert) => {
+    let offset = numberMoves(totalNumberCypher);
+    textToConvert = textToConvert.toUpperCase();
+    let arrayOfCharacters = textToConvert.split('');
+    let code = '';
+    for (let i = 0; i < arrayOfCharacters.length; i++) {
+      let codeform;
+      let codeAscii = arrayOfCharacters[i].charCodeAt(0);
+      if (codeAscii < 65 || codeAscii > 90 && codeAscii < 97 || codeAscii > 122 && codeAscii < 127) {
+        codeform = arrayOfCharacters[i];
+      } else {
+        if (codeAscii + offset >= 65 + maxLetterNumber) {
+          codeform = 65 + ((codeAscii + offset) - (maxLetterNumber + 65));
+        } else if (codeAscii + offset < 65) {
+          codeform = (maxLetterNumber + 65) - (65 - (codeAscii + offset));
+        } else {
+          codeform = (codeAscii + offset);
+        }
+      }
+      code += String.fromCharCode(codeform);
+    }
+    return code;
+  },
+
+  decode: (totalNumberCypher, textToConvert) => {
+    let offset = numberMoves(totalNumberCypher);
+    textToConvert = textToConvert.toUpperCase();
+    let arrayOfCharacters = textToConvert.split('');
+    let code = '';
+    for (let i = 0; i < arrayOfCharacters.length; i++) {
+      let codeform;
+      if (arrayOfCharacters[i] == ' ') {
+        codeform = arrayOfCharacters[i];
+      } else {
+        let codeAscii = arrayOfCharacters[i].charCodeAt(0);
+        if (codeAscii - offset < 65) {
+          codeform = (maxLetterNumber + 65) - (65 - (codeAscii - offset));
+        } else if (codeAscii - offset > 65 + maxLetterNumber) {
+          codeform = 65 + ((codeAscii + offset) + (maxLetterNumber + 65));
+        } else {
+          codeform = (codeAscii - offset);
+        }
+      }
+      code += String.fromCharCode(codeform);
+    }
+    return code;
   }
-}
-
-
-function decode(number, text) {
-  let newNumber = numberMoves(number);
-
-}
+};
 
 function numberMoves(numberToConvert) {
   numberToConvert = parseInt(numberToConvert);
--- src/index.js
+++ src/index.js
@@ -1,10 +1,36 @@
-let messageTo = document.getElementById("yourInput");
-let number = document.getElementById("numberCode");
-let coder = new cipher();
+let isCoding = true;
+let buttonFun = document.getElementById('buttonFuntional');
+let buttonToChange = document.getElementById('buttonToChange');
+let inputLabel = document.getElementById('messageText');
+let resultLabel = document.getElementById('codeText');
+let resultArea = document.getElementById('yourResult');
 
-let result = coder.endcode(number, messageTo);
+let messageText = 'Mensaje';
+let codeText = 'Codigo';
 
+buttonToChange.addEventListener('click', ()=>{
+  isCoding = !isCoding;
+  if(isCoding){
+    //set message to original state
+    inputLabel.innerHTML = messageText;
+    resultLabel.innerHTML = codeText;
+    buttonFun.value = 'codificar';
+  } else {
+    //set message to invert place
+    inputLabel.innerHTML = codeText;
+    resultLabel.innerHTML = messageText;
+    buttonFun.value = 'decodificar';
+  }
+})
 
-function cambiarTextos(){
-			texto.innerHTML=texto.innerHTML=="Mensaje"?"Codigo":"Mensaje";
-		}
+buttonFun.addEventListener('click',()=>{
+  let numberCrypting = document.getElementById("numberCode").value;
+  let textToConvert = document.getElementById("yourInput").value;
+  if(isCoding){
+    //llamar al metododo codificar e imprimirlo
+    resultArea.value = window.cipher.encode(numberCrypting, textToConvert);
+  } else {
+    //llamar al metododo decodificar e imprimirlo
+    resultArea.value = window.cipher.decode(numberCrypting, textToConvert);
+  }
+})
